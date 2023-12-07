e400d81edefae50e2b665d8c85aeeef5056fbce3
bamboolsu <bamboolsu@gmail.com>
2023-12-07 11:35:10 +0800

    modify

--- leodetect.sh
+++ leodetect.sh
@@ -2,5 +2,9 @@
 
 conda activate py39-torch1.12.1
 
-python detect.py --weights yolov7.pt --conf 0.25 --img-size 640 --source inference/images/horses.jpg 
+#official detect
+#python detect.py --weights yolov7.pt --conf 0.25 --img-size 640 --source inference/images/20231207103708.jpg 
+
+#leo detect --conf 0.45 --iou 0.10
+python  detect.py --weights ./runs/train/yolov7-epoch100-batch3-worker6/weights/best.pt --source  ./test/ 
 
