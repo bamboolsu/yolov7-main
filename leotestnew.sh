da724dd805dffa8f874a9af2f27022f68a7d11c3
bamboolsu <bamboolsu@gmail.com>
2023-12-07 09:47:15 +0800

    modify shell

--- leotestnew.sh
+++ leotestnew.sh
@@ -3,4 +3,4 @@
 conda activate py39-torch1.12.1
 
 
-python test.py --data data/coco.yaml --img 640 --batch 32 --conf 0.001 --iou 0.65 --device 0,1  --weights yolov7.pt --name yolov7_640_val_test &
+python test.py --data data/leo_cust.yaml --img 640 --batch 32 --conf 0.001 --iou 0.65 --device 0,1  --weights yolov7.pt --name yolov7_640_val_test &
